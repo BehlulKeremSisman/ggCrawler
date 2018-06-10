348306c615bd1c972cb6c1e0ab05e9cfd7433bf2
behlulkeremsisman <behlulkeremsisman@BKS.local>
2018-06-11 00:18:08 +0300

    First json output

--- gittigidiyor.py
+++ gittigidiyor.py
@@ -1,9 +1,28 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
-r = requests.get("https://profil.gittigidiyor.com/arcelik")
-soup = BeautifulSoup(r.content, "lxml")
+url = "https://profil.gittigidiyor.com/bayanjasmin"
+r = requests.get(url)
+soup = BeautifulSoup(r.content, "html.parser")
 
-storeName = soup.find("div", attrs={"class" : "short_info"})
+review = soup.find("span", attrs={"class" : "total_evaluation"})
+rate = soup.find("span", attrs={"class" : "positive_percentage"})
+criteria = soup.find("div", attrs={"class" : "col_width-8of16_color_8e"})
 
-print(storeName.span.text)
+#ornek kullanim: filmTablosu = soup.find("table".attrs={"class:"ustcizgi"}).select("tr:nth-of-type(2) > td > table:nth-of-type(3) > tr")
+#bu ornek class degeri ustcizgi olan table altindaki 2.tr'nin altindaki td'nin altindaki 3.table'in altindaki tr'yi cekmeye yarar
+numberOfReviews = review.text.strip()
+rateScore = rate.text.strip()
+criterias = criteria
+
+print("Number of reviews in the last 12 months: {}\nPositive score rate during the last 12 months: {}\nCriterias: {}".format(
+    numberOfReviews,
+    rateScore,
+    criterias
+))
+
+file = open("output.json", "w")
+output = {"magaza": [{"Number of reviews":numberOfReviews, "Positive score rate":rateScore, "criterias":criterias}]}
+json.dump(output, file)
+file.close()
