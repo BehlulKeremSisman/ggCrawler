c8001285dd6470082d42188b4af060ac23b4454c
behlulkeremsisman <behlulkeremsisman@BKS.local>
2018-06-11 12:43:59 +0300

    new parameters

--- gittigidiyor.py
+++ gittigidiyor.py
@@ -2,27 +2,57 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-url = "https://profil.gittigidiyor.com/bayanjasmin"
+url = "https://profil.gittigidiyor.com/teknorya"
 r = requests.get(url)
-soup = BeautifulSoup(r.content, "html.parser")
+soup = BeautifulSoup(r.content, "lxml")
 
 review = soup.find("span", attrs={"class" : "total_evaluation"})
 rate = soup.find("span", attrs={"class" : "positive_percentage"})
 criteria = soup.find("div", attrs={"class" : "col_width-8of16_color_8e"})
+badge = str(soup.find("div", attrs={"class" : "badge_container"}))
+
+if badge.find("19") != -1:
+    badge2 = "Cok basarili"
+elif badge.find("18") != -1:
+    badge2 = "Basarili"
+else:
+    badge2 = "Rozet yok"
 
 #ornek kullanim: filmTablosu = soup.find("table".attrs={"class:"ustcizgi"}).select("tr:nth-of-type(2) > td > table:nth-of-type(3) > tr")
 #bu ornek class degeri ustcizgi olan table altindaki 2.tr'nin altindaki td'nin altindaki 3.table'in altindaki tr'yi cekmeye yarar
+
 numberOfReviews = review.text.strip()
 rateScore = rate.text.strip()
 criterias = criteria
 
-print("Number of reviews in the last 12 months: {}\nPositive score rate during the last 12 months: {}\nCriterias: {}".format(
-    numberOfReviews,
-    rateScore,
-    criterias
-))
-
 file = open("output.json", "w")
-output = {"magaza": [{"Number of reviews":numberOfReviews, "Positive score rate":rateScore, "criterias":criterias}]}
+output = {"Teknorya": {"Number of reviews":numberOfReviews, "Positive score rate":rateScore, "criterias":criterias, "badge": badge2}}
 json.dump(output, file)
 file.close()
+
+urlProduct = "https://www.gittigidiyor.com/arama/?satici=teknorya"
+r2 = requests.get(urlProduct)
+soup2 = BeautifulSoup(r2.content, "lxml")
+
+file = open("urunler.json", "w")
+
+productCount = soup2.find_all("p", attrs={"itemprop" : "price"})
+i=0
+
+for i in range(len(productCount)):
+    productName = soup2.find_all("span", attrs={"itemprop" : "name"})[i].text
+    productPrice = soup2.find_all("p", attrs={"itemprop" : "price"})[i].text.strip()
+    urunler = {"TeknoryaUrunler": {"Name":productName, "Price":productPrice}}
+    json.dump(urunler, file)
+
+file.close()
+
+
+print("Number of reviews in the last 12 months: {}\nPositive score rate during the last 12 months: {}\nCriterias: {}\nBadge: {}\nProducts: {}\nPrice: {}".format(
+    numberOfReviews,
+    rateScore,
+    criterias,
+    badge2,
+    productName,
+    productPrice
+))
